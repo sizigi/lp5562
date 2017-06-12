6ad67a584619dd5a9d1aa8d4cb273a6f6a4fd783
Cory Li <coryli@mit.edu>
2017-06-12 15:50:21 -0700

    prepwork for npm (fix main & typings)

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+/**
+ * Assembles the provided program into a array of 48 WORDs representing
+ * the memory map of the LP5562's program space.
+ */
+export function assemble(program:string) : Uint16Array;
